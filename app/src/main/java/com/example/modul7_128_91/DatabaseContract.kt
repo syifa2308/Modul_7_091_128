71364ed942d1662ed983856c837e1b0acfc5cace
syifa2308 <khairunisasyifa08@gmail.com>
2024-11-29 09:56:12 +0700

    Penambahan DatabaseContract.kt dan DatabaseHelper.kt

--- app/src/main/java/com/example/modul7_128_91/DatabaseContract.kt
+++ app/src/main/java/com/example/modul7_128_91/DatabaseContract.kt
@@ -1,4 +1,15 @@
 package com.example.modul7_128_91
 
-class DatabaseContract {
+import android.provider.BaseColumns
+
+internal class DatabaseContract {
+    internal class  HomeworkColumns : BaseColumns {
+        companion object {
+            const val TABLE_NAME = "homework"
+            const val _ID = "_id"
+            const val TITLE = "title"
+            const val DESCRIPTION = "description"
+            const val DATE = "date"
+        }
+    }
 }
