e2c49e32bf4f2bcee1c839d706f0bd117e139843
syifa2308 <khairunisasyifa08@gmail.com>
2024-11-29 10:04:38 +0700

    Penambahan Homework.kt

---
+++ app/src/main/java/com/example/modul7_128_91/Homework.kt
@@ -0,0 +1,11 @@
+package com.example.modul7_128_91
+
+import android.os.Parcelable
+
+@Parcelize
+data class Homework(
+    var id: Int = 0,
+    var title: String? = null,
+    var description: String? = null,
+    var date: String? = null
+) : Parcelable
